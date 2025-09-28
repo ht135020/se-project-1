f93cda8b2216846183432ce4fa1a4d065fd5e81d
ht135020 <hassanein1812@gmail.com>
2025-09-28 11:35:39 +0300

    pushing a new file

--- stars.py
+++ stars.py
@@ -3,4 +3,3 @@ i = 0 # i should define i and put a start point (0)
 while i <= 5: # i need the 5th line because of this i put <=
     print("*" * i)  # my code should print in first loop print 1 star because i=1, 2nd loop i=2 * (*) =** , 3rd loop i=3 *(*) = *** ,..... 
     i+=1        # in each loop i should add 1 after doing the all code above it  , and i choose while loop because i know the range of the loop
-    print("*")
